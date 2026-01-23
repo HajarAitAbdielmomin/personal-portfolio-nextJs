a863ce81640bec52863ba2579a74a5f86f1fae58
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2026-01-23 18:40:46 +0100

    call the Education component in the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HomePage from './components/sections/home-page/HomePage';
+import Education from "@/app/components/sections/education/Educations";
 
 export default function Page() {
     return (
         <main className="bg-gray-50">
             <HomePage />
+            <Education />
 
             {/* Add other sections here as you build them */}
         </main>
