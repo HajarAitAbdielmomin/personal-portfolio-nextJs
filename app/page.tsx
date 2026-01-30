0d9920fc76cd60c3bdc22aca1aa8f69a86e59e53
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2026-01-30 17:13:37 +0100

    import the Internship component to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import HomePage from './components/sections/home-page/HomePage';
 import Education from "@/app/components/sections/education/Educations";
+import Internships from "@/app/components/sections/internships/Internships";
 
 export default function Page() {
     return (
         <main className="bg-gray-50">
             <HomePage />
             <Education />
-
+            <Internships />
             {/* Add other sections here as you build them */}
         </main>
     );
